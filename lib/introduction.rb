aff64b6a724f6af02896794c8e860ded29881a1f
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-16 03:14:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end 
 
-def introduction_with_language (name, langauge)
+def introduction_with_language ("Dan", "Ember.js")
   puts "Hi, my name is #{name} and I am learning to program in #{langauge}"
 end
